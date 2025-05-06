cd4a64265fe26c1c4f02679654d8525ebfa0994b
Cody Brett (08001547) <BRETCW1@student.op.ac.nz>
2025-05-06 16:37:03 +1200

    populated repo

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TestRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+			
+			// Cody's area
+			Console.WriteLine("Hi my name is Cody");
+			
+			
+			
+			
+			// Thomas's area
+			
+			
+
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.WriteLine("Thomas Visser");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+			
+
+
+			
+			// Logan's Area
+
+
+
+			Console.WriteLine("\n\nIch bin Logan");
+			
+			Console.ReadLine();
+
+        }
+    }
+}
