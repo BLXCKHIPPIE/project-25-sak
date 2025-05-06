c3d15dfe319d7256b5de4a285a85cdce05179df3
Cody Brett (08001547) <BRETCW1@student.op.ac.nz>
2025-05-07 11:24:25 +1200

    Cleared Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,35 +1,10 @@
-﻿namespace TestRepo
+﻿namespace Wah
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-			
-			// Cody's area
-			Console.WriteLine("Hi my name is Cody");
-			
-			
-			
-			
-			// Thomas's area
-			
-			
-
-			Console.ForegroundColor = ConsoleColor.White;
-			Console.BackgroundColor = ConsoleColor.Red;
-			Console.WriteLine("Thomas Visser");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.Black;
-			
-
-
-			
-			// Logan's Area
-
-
-
-			Console.WriteLine("\n\nIch bin Logan");
 			
 			Console.ReadLine();
 
