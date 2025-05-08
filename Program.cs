f27354e5cf7bfc4c46dbcfc3d3b274dfe1136980
Cody Brett (08001547) <BRETCW1@student.op.ac.nz>
2025-05-08 16:22:18 +1200

    Added functional menu

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+        
+        static void Main() // Declare main method
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Keep me empty pls"); // placeholder
 			
-			Console.ReadLine();
+			Console.ReadLine(); // pause
+            menu(); // calls menu
+
+        }
+
+
+        static void menu() // Declare title method
+        {
+
+            int decision; // define decision variable
+            do
+            {
+
+                Console.WriteLine("Menu\n1. New Game\n2. Credits\n3. Exit"); // presenting options
+                decision = Convert.ToInt32(Console.ReadLine());// taking user input and assigning to decision
+                Console.Clear(); // clearing screen
+                switch (decision) // switch statement for different options
+                {
+
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                }
+                Console.Clear(); // clear screen
+            }
+            while (decision != 0); // exit command - placeholder to be worked on
+            Console.ReadLine(); // pause
+
 
         }
     }
